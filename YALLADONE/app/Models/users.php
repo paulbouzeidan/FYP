1497c98979ec88be8c9b51e5bb98051f7e47953a
paul zaiden <paul7zaiden2@gmail.com>
2024-05-28 17:19:21 +0300

    modifying users model

--- YALLADONE/app/Models/users.php
+++ YALLADONE/app/Models/users.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class users extends Authenticatable
+class users extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
